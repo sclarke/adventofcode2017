76ea8d7b38c7f2cc7e6d810eddfab0efbe7cbee9
Sean Clarke <Sean.Clarke@nasa.gov>
2017-12-03 21:27:29 -0800

    d4 completed (including some inline tests)

--- d04.py
+++ d04.py
@@ -9,10 +9,26 @@ test_input = ["aa bb cc dd ee",
 with open('d04_input.txt') as f:
     puzzle_input = f.readlines()
 
+
 def check_line(line):
     count = Counter(line.split())
+    return max(count.values()) == 1
 
-    return True if max(count.values()) == 1 else False
 
-print(sum(check_line(phrase) for phrase in test_input))
-print(sum(check_line(phrase) for phrase in puzzle_input))
+print("Test Text", sum(check_line(phrase) for phrase in test_input), '\n')
+print("Puzzle Text", sum(check_line(phrase) for phrase in puzzle_input), '\n')
+
+
+def check_line_2(line):
+    count = Counter([''.join(sorted(word)) for word in line.split()])
+    return max(count.values()) == 1
+
+
+print("abcde fghij", check_line_2("abcde fghij"))
+print("abcde xyz ecdab", check_line_2("abcde xyz ecdab"))
+print("a ab abc abd abf abj", check_line_2("a ab abc abd abf abj"))
+print("iiii oiii ooii oooi oooo", check_line_2("iiii oiii ooii oooi oooo"))
+print("oiii ioii iioi iiio", check_line_2("oiii ioii iioi iiio"))
+
+print()
+print("Puzzle Text", sum(check_line_2(phrase) for phrase in puzzle_input), '\n')
